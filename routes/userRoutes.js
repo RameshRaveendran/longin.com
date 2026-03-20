20d92dc37ce5a08af54a7345ecf0bdd957a03404
Ramesh Raveendran <rameshraveendranm@gmail.com>
2026-03-20 14:10:18 +0530

    Auth middleware for JWT verification and role-based authorization

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const router = express.Router();
 
+const { protect } = require("../middleware/authMiddleware");
 const {
   registerUser,
   loginUser,
 } = require("../controllers/userController");
 
+// example protected route
+router.get("/profile", protect, (req, res) => {
+  res.send(req.user);
+});
 router.post("/register", registerUser);
 router.post("/login", loginUser);
 
