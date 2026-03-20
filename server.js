9f44c5d02d61275d23bfa9ad9c2ff35b59577cb0
Ramesh Raveendran <rameshraveendranm@gmail.com>
2026-03-20 13:29:09 +0530

    mongodb connection check

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
 
+
 dotenv.config();
 
 const app = express();
@@ -11,6 +12,10 @@ app.use(express.urlencoded({ extended: true }));
 // Routes
 const userRoutes = require("./routes/userRoutes");
 const adminRoutes = require("./routes/adminRoutes");
+const connectDB = require("./config/db");
+
+// connect database
+connectDB();
 
 app.use("/api/users", userRoutes);
 app.use("/api/admin", adminRoutes);
