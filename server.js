ab69375bb8d09f26f3853a1acf1683f489b456cf
Ramesh Raveendran <rameshraveendranm@gmail.com>
2026-03-21 15:16:32 +0530

    views setup added

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const path = require("path");
+
+
 
 
 dotenv.config();
@@ -8,6 +11,9 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 // Routes
 const userRoutes = require("./routes/userRoutes");
