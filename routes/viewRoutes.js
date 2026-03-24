cdd1c0f9123dbaa3f920f195f2e2b5950bb26018
Ramesh Raveendran <rameshraveendranm@gmail.com>
2026-03-24 21:16:42 +0530

    register updated backend logic check

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -19,18 +19,38 @@ router.get("/register", (req, res) => res.render("auth/register"));
 router.get("/login", (req, res) => res.render("auth/login"));
 
 // REGISTER
-router.post("/register", async (req, res) => {
-  const { name, email, password } = req.body;
+router.post('/register', async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
 
-  const hashed = await hashPassword(password);
+    // Manual validation (BEST PRACTICE)
+    if (!name || !email || !password) {
+      return res.status(400).json({
+        message: "All fields are required"
+      });
+    }
+    // CHECK existingUser
+    const existingUser = await User.findOne({ email });
 
-  const user = await User.create({
-    name,
-    email,
-    password: hashed,
-  });
+    if (existingUser) {
+      return res.status(400).json({
+        message: "User already exists"
+      });
+      
+    }
+    
+    const user = await User.create(req.body);
 
-  res.redirect("/login");
+    res.status(201).json({
+      message: "User created successfully",
+      user
+    });
+
+  } catch (error) {
+    res.status(400).json({
+      message: error.message
+    });
+  }
 });
 
 // LOGIN
