789fda4af1cf83c2617f6c26fec5f4c6d8613415
Ramesh Raveendran <rameshraveendranm@gmail.com>
2026-03-25 13:08:30 +0530

    route check

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -4,7 +4,6 @@ const router = express.Router();
 const { getAllUsers , updateUser, deleteUser } = require("../controllers/adminController");
 const { protect, authorize } = require("../middleware/authMiddleware");
 
-// only admin can access
 router.get("/users", protect, authorize("admin"), getAllUsers);
 router.put("/users/:id", protect, authorize("admin"), updateUser);
 router.delete("/users/:id", protect, authorize("admin"), deleteUser);
