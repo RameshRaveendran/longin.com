59d6f0c8abcc81e47e5577d42f046d0cf8c957c0
Ramesh Raveendran <rameshraveendranm@gmail.com>
2026-03-27 20:46:37 +0530

    password hash page detailing

--- utils/hashPassword.js
+++ utils/hashPassword.js
@@ -1,12 +1,27 @@
+// ============================
+// 1. Bcrypt Library Import
+// ============================
 const bcrypt = require("bcryptjs");
 
+
+// ============================
+// 2. Hash Password Function
+// ============================
 const hashPassword = async (password) => {
   const salt = await bcrypt.genSalt(10);
   return await bcrypt.hash(password, salt);
 };
 
+
+// ============================
+// 3. Compare Password Function
+// ============================
 const comparePassword = async (enteredPassword, storedPassword) => {
   return await bcrypt.compare(enteredPassword, storedPassword);
 };
 
+
+// ============================
+// 4. Export Functions
+// ============================
 module.exports = { hashPassword, comparePassword };
