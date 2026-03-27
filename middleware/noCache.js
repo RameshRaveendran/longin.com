e5a6283ccb53de672027daed3b4e108f4f5b64b5
Ramesh Raveendran <rameshraveendranm@gmail.com>
2026-03-27 20:47:25 +0530

    nocache  detailing

--- middleware/noCache.js
+++ middleware/noCache.js
@@ -1,8 +1,15 @@
-// for prevent browser
+// ============================
+// 1. No Cache Middleware Function
+// ============================
+// Prevent browser from caching protected pages
 
 const noCache = (req, res, next) => {
   res.set("Cache-Control", "no-store, no-cache, must-revalidate, private");
   next();
 };
 
+
+// ============================
+// 2. Export Middleware
+// ============================
 module.exports = noCache;
